befea918d65a28c1a7e402d626c22d44eed19951
Amit <amityanay5@gmail.com>
2021-11-18 13:49:13 +0200

    Update SpotiTest.py

--- cogs/SpotiTest.py
+++ cogs/SpotiTest.py
@@ -11,12 +11,6 @@ class SpotiTest(commands.Cog):
         self.client = client
         self.spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-    @commands.command()
-    async def SpotiTestAlbum(self, ctx, search):
-        artist = self.spotify.artist_top_tracks(search)
-        print(artist['tracks'][0]['artists'][0]['name'])
-        print(artist['tracks'][0]['name'])
-
 
 
 def setup(client):
