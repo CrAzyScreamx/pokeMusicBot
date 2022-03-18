4bb070e56893cbe5560364e2aee86fde07692ab7
Amit <amityanay5@gmail.com>
2022-03-18 17:20:12 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,6 +20,6 @@ async def re(ctx):
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
-        await client.load_extension(f'cogs.{filename[:-3]}')
+        client.load_extension(f'cogs.{filename[:-3]}')
 
 client.run(os.environ.get("TOKEN"))
